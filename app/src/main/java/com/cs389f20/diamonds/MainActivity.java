a0088f7f1f86e008bdb164973160d6ee72bcf2a2
siralpega <6191199+siralpega@users.noreply.github.com>
2020-09-17 21:21:53 -0400

    Initial commit

---
+++ app/src/main/java/com/cs389f20/diamonds/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cs389f20.diamonds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
