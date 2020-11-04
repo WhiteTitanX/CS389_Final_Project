a217a3c2a65ef024b45162baa06f0b76a5bf3cc3
Bryan Johnsen <34360340+bryjohn@users.noreply.github.com>
2020-11-03 21:13:38 -0500

    basic graph of past data

--- app/src/main/java/com/cs389f20/diamonds/GraphActivity.java
+++ app/src/main/java/com/cs389f20/diamonds/GraphActivity.java
@@ -4,30 +4,94 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
+
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class GraphActivity extends AppCompatActivity {
 
-    private TextView textFlip;
     private Building building;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
-
+        // used for timestamp formatting
+        final SimpleDateFormat adf = new SimpleDateFormat("hh:mm a");
         Intent intent = getIntent();
         building = (Building) intent.getSerializableExtra(BuildingSelectActivity.EXTRA_BUILDING);
         assert building != null;
         setTitle(getString((R.string.graph_title), building.name));
 
-        textFlip = findViewById(R.id.textFlip);
+        PastCount[] data = building.getPastArray(60);
+        if (data == null) //TODO Make error screen?
+        {
+            Log.e(HistoryActivity.class.getSimpleName(), "There is no past data for " + building.name);
+            Toast.makeText(getApplicationContext(), "Error: No past data for " + building.name, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+
+        GraphView graph = findViewById(R.id.graph);
+        // set date label formatter for x axis format (hh:mm am/pm)
+        graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter()
+        {
+            @Override
+            public java.lang.String formatLabel(double value, boolean isValueX) {
+                if(isValueX)
+                {
+                    return adf.format(new Date((long)value));
+                }
+                else{
+                    return super.formatLabel(value, isValueX);
+                }
+
+            }
+        });
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3);//fix the number of default x axis labels 
+        DataPoint[] points = new DataPoint[24];
+        double y_max = 100;//placeholder
+        for(int i = 0; i < points.length; i++){
+            points[i] = new DataPoint(data[i].getDate(),data[i].getPeople());
+            if(data[i].getPeople() > y_max){
+                y_max = data[i].getPeople();
+            }
+        }
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(points);
+
+
+        // set manual x bounds
+        graph.getViewport().setMinX(data[0].getDate().getTime());
+        graph.getViewport().setMaxX(data[23].getDate().getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
+        //set manual y bounds
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(y_max + 10);
+        graph.getViewport().setYAxisBoundsManual(true);
+        //prevent GraphView from rounding x and y values
+        graph.getGridLabelRenderer().setHumanRounding(false);
+        //enable pinch and zoom for graph NOTE: Please try on actual phone
+        graph.getViewport().setScalable(true);
+        graph.getViewport().setScalableY(true);
+
+        //TODO "tap listener on data" so that each point can be clicked on to show data
+
+        //TODO "Style options"
+        graph.addSeries(series);
+
+
+
     }
 
     @Override
@@ -39,20 +103,5 @@ public class GraphActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void createGraph(View v) {
-        PastCount[] data = building.getPastArray(60);
-        if (data == null) //You can turn this into an error screen instead of just a toast, or just keep it like this
-        {
-            Log.e(HistoryActivity.class.getSimpleName(), "There is no past data for " + building.name);
-            Toast.makeText(getApplicationContext(), "Error: No past data for " + building.name, Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
 
-        int people = data[0].getPeople();
-        Date date = data[0].getDate();
-        String dateStr = data[0].getDate().toString();
-
-        textFlip.setVisibility(View.VISIBLE);
-    }
 }
